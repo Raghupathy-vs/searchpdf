e768005a46dd344098adc6a5523b892fa5b61132
Raghupathy-vs <raghupathym7@outlook.com>
2025-11-25 15:32:12 +0530

    create new file

--- server.js
+++ server.js
@@ -145,4 +145,5 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/home.html");
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
